22e113112809e79a28ccdc6d37187e5c2e9cec49
Luis Puentes <luispuentesvega@gmail.com>
2018-10-25 23:10:47 +0700

    Deleting the contructor React 16

--- src/App.js
+++ src/App.js
@@ -4,17 +4,13 @@ import Header from './components/Header';
 import Main from './components/Main';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            showCreate: false
-        }
+    state = {
+        showCreate: false
     }
 
     showCreateHandler = show => {
         this.setState({showCreate: !this.state.showCreate});
     };
-
 
     render() {
         const { showCreate } = this.state;
