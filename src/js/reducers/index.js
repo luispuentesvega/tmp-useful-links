5948e7c215f5853669a5dceb52cf08568124a30f
Luis Puentes <luispuentesvega@gmail.com>
2019-01-03 20:37:45 +0700

    Adding some css styles

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,27 +1,38 @@
 import * as actionTypes from "../constants/action-types";
 
 const initialState = {
-    links: []
-}
+    links: [],
+    groups: []
+};
 
 const rootReducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.ADD_LINK:
-            return Object.assign({}, state, {
-                links: state.links.concat(action.payload)
-            });
+            let updatedGroups ={ ...state.groups };
+            updatedGroups[action.link.topic].push(action.link);
+            return {
+                ...state,
+                links: state.links.concat(action.link),
+                groups: updatedGroups
+            };
         case actionTypes.DATA_LOADED:
-            return Object.assign({}, state, {
-                links: action.payload
-            });
+            return {
+                ...state,
+                links: action.links
+            };
         case actionTypes.SET_TOPICS:
             return {
                 ...state,
                 topics: action.topics
-            }
+            };
+        case actionTypes.DATA_GROUPS_LOADED:
+            return {
+                ...state,
+                groups: action.groups
+            };
         default:
             return state;
     }
-}
+};
 
 export default rootReducer;
