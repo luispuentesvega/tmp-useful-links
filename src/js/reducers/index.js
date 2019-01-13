e4c0dac58307f41291f75d31b4f5f376270f9497
Luis Puentes <luispuentesvega@gmail.com>
2019-01-13 19:14:08 +0700

    Added pretier

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,34 +1,34 @@
-import * as actionTypes from "../constants/action-types";
+import * as actionTypes from '../constants/action-types';
 
 const initialState = {
     links: [],
-    groups: []
+    groups: [],
 };
 
 const rootReducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.ADD_LINK:
-            let updatedGroups ={ ...state.groups };
+            let updatedGroups = { ...state.groups };
             updatedGroups[action.link.topic].push(action.link);
             return {
                 ...state,
                 links: state.links.concat(action.link),
-                groups: updatedGroups
+                groups: updatedGroups,
             };
         case actionTypes.DATA_LOADED:
             return {
                 ...state,
-                links: action.links
+                links: action.links,
             };
         case actionTypes.SET_TOPICS:
             return {
                 ...state,
-                topics: action.topics
+                topics: action.topics,
             };
         case actionTypes.DATA_GROUPS_LOADED:
             return {
                 ...state,
-                groups: action.groups
+                groups: action.groups,
             };
         default:
             return state;
